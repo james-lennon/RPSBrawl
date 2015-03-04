443395d2d5a28d742cdc500eeece389d0f63c9b8
James Lennon <jameslennon321@gmail.com>
2015-03-03 16:06:48 -0800

    Added Server Socket, preliminary connection accept

--- RPSBrawl/src/com/csclub/rpsbattle/Server.java
+++ RPSBrawl/src/com/csclub/rpsbattle/Server.java
@@ -1,5 +1,33 @@
 package com.csclub.rpsbattle;
 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+
+	private int portNumber;
+	private ServerSocket serverSocket;
+	private boolean shouldRun;
+
+	public Server(int port) {
+		port = portNumber;
+	}
+
+	public void start() throws Exception {
+		shouldRun = true;
+		serverSocket = new ServerSocket(portNumber);
+
+		while (shouldRun && !serverSocket.isClosed()) {
+			Socket connection = serverSocket.accept();
+			PrintWriter out = new PrintWriter(connection.getOutputStream(),
+					true);
+			Scanner in = new Scanner(new InputStreamReader(
+					connection.getInputStream()));
+			
+		}
+	}
 
 }
