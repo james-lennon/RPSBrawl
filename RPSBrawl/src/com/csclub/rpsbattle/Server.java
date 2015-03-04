1f2c998741d171d1da9f9ae07b065d97da53ac4d
James Lennon <jameslennon321@gmail.com>
2015-03-03 16:18:52 -0800

    Fixed Eli's stuff

--- RPSBrawl/src/com/csclub/rpsbattle/Server.java
+++ RPSBrawl/src/com/csclub/rpsbattle/Server.java
@@ -1,39 +1,39 @@
-//package com.csclub.rpsbattle;
-//
-//import java.io.InputStreamReader;
-//import java.io.PrintWriter;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//import java.util.Scanner;
-//
-//public class Server {
-//
-//	private int portNumber;
-//	private ServerSocket serverSocket;
-//	private boolean shouldRun;
-//	private int currentID;
-//
-//	public Server(int port) {
-//		port = portNumber;
-//	}
-//
-//	public void start() throws Exception {
-//		shouldRun = true;
-//		currentID = 1;
-//		serverSocket = new ServerSocket(portNumber);
-//
-//		while (shouldRun && !serverSocket.isClosed()) {
-//			Socket connection = serverSocket.accept();
-//			PrintWriter out = new PrintWriter(connection.getOutputStream(),
-//					true);
-//			Scanner in = new Scanner(new InputStreamReader(
-//					connection.getInputStream()));
-//			
-//		}
-//	}
-//	
-//	public synchronized int getNextID(){
-//		return currentID++;
-//	}
-//
-//}
+package com.csclub.rpsbattle;
+
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+	private int portNumber;
+	private ServerSocket serverSocket;
+	private boolean shouldRun;
+	private int currentID;
+
+	public Server(int port) {
+		port = portNumber;
+	}
+
+	public void start() throws Exception {
+		shouldRun = true;
+		currentID = 1;
+		serverSocket = new ServerSocket(portNumber);
+
+		while (shouldRun && !serverSocket.isClosed()) {
+			Socket connection = serverSocket.accept();
+			PrintWriter out = new PrintWriter(connection.getOutputStream(),
+					true);
+			Scanner in = new Scanner(new InputStreamReader(
+					connection.getInputStream()));
+			
+		}
+	}
+	
+	public synchronized int getNextID(){
+		return currentID++;
+	}
+
+}
